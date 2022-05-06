8ee3fa957924457f9d7ec910ed86ae9859866c4b
osekman <osekman.02@gmail.com>
2022-05-07 00:44:11 +0300

    ..

--- basic/main.go
+++ basic/main.go
@@ -11,9 +11,18 @@ func main() {
 
 	str, sayi, kosul := "ali", 12, false // çoklu değişken tanımlama
 
+	const x = 3
+	const y = 5.8
+
 	fmt.Printf("%#v %T, ", str, str)
 	fmt.Printf("%#v %T, ", sayi, sayi)
 	fmt.Printf("%#v %T, ", kosul, kosul)
+
+	fmt.Println()
+	fmt.Println("--- typless const ---")
+	fmt.Printf("%#v %T, %#v %T, ", x, x, y, y)
+	fmt.Println()
+	fmt.Printf("%#v %T, ", x+y, x+y)
 }
 
 func yaz() {

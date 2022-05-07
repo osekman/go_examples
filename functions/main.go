2f8e01ecef2eca1aa9805d94035b81aa4ae9ba40
osekman <osekman.02@gmail.com>
2022-05-07 23:34:05 +0300

    ..

--- functions/main.go
+++ functions/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 )
@@ -13,6 +14,13 @@ func main() {
 	fmt.Printf("sonuc > %v", basic3(5, 3))
 
 	read()
+
+	sn, err := basic4(1, 3)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("sonuc > %v", sn)
+	}
 }
 
 func read() {
@@ -37,4 +45,13 @@ func basic3(x, y float32) float32 {
 
 	fmt.Printf("basic3 degerler %v, %v, ", x, y)
 	return x / y
+}
+
+func basic4(x, y float32) (float32, error) {
+
+	fmt.Printf("basic3 degerler %v, %v, ", x, y)
+	if x/y < 1 {
+		return 0, errors.New("x y den küçük")
+	}
+	return x / y, nil
 }

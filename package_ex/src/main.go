78c6d4e3ed6542f9d95a6be92f9673e397c30f85
osekman <osekman.02@gmail.com>
2022-05-08 17:38:48 +0300

    ..

--- package_ex/src/main.go
+++ package_ex/src/main.go
@@ -2,11 +2,16 @@ package main
 
 //local paketleri import etmek için
 //go mod init package_name, komutu ile go.mod dosyasının oluşmasını sağlıyoruz.
+//ps klasörü içinde iki tane p1.go ve p2.go isimli dosyalar var, paket isimleri ps olduğu icin aynı paketin farklı dosyaları gibi kullanıyoz
+//ps in içindeki mycelc ise ayrı bir klasörde olduğu için farklı bir paket ismi alıyor. dosya ismi önemli değil, dosyanın başındaki paket adı ile bulunduğu klasör aynı olmalı, javadaki gibi
 import (
 	"fmt"
-	"package_ex/src/mycelc"
+	"package_ex/src/ps"
+	"package_ex/src/ps/mycelc"
 )
 
 func main() {
-	fmt.Printf("cc %v", mycelc.GetCelc(20))
+	fmt.Printf("cc %v \n", mycelc.GetCelc(20))
+	fmt.Printf("conv SecondToMinute %v => %v \n", 600, ps.SecondToMinute(600))
+	fmt.Printf("conv MinuteToSecond %v => %v \n", 10, ps.MinuteToSecond(10))
 }
---
+++ package_ex/src/ps/p1.go
@@ -0,0 +1,5 @@
+package ps
+
+func SecondToMinute(i int32) int32 {
+	return i / 60
+}
